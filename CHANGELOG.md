8dfeb5f2e91b1d36c93efa81885759400b983dd0
Yoeri van Zwol <yoeriz123@gmail.com>
2025-05-04 18:13:33 +0200

    updated exports

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# 0.0.3
+ - Updated exports
+
 # 0.0.2
  - Added debug BonError
 
--- lib/bon_notifiers.dart
+++ lib/bon_notifiers.dart
@@ -3,6 +3,8 @@ library;
 export 'src/interfaces/async_listenable.dart';
 export 'src/notifiers/async_notifier.dart';
 export 'src/widgets/async_listenable_builder.dart';
+export 'src/mixins/error_notifier.dart';
+export 'src/mixins/loading_notifier.dart';
 
 class BonError extends Error {
   BonError({required this.message});
